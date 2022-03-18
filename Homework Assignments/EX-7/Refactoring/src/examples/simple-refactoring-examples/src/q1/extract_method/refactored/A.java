4fef228ad8e16d0312701b7b5e8f34a176cc6c9f
Zach Randolph <zrandolph977@gmail.com>
2022-03-18 13:29:31 -0500

    Committing homework 7
    Committing homework 7 on refactoring. They don't run because the packages are all messed up from when I imported it from the examples online but if that issue was resolved then they would all run correctly.

---
+++ Homework Assignments/EX-7/Refactoring/src/examples/simple-refactoring-examples/src/q1/extract_method/refactored/A.java
@@ -0,0 +1,49 @@
+
+import java.util.List;
+
+public class A {
+   Node m1(List<Node> nodes, String p) {
+      // TODO: Your answer
+      // other implementation
+	  extractedMethod(nodes, p);
+      return null;
+   }
+
+   Edge m2(List<Edge> edgeList, String p) {
+      // TODO: Your answer
+      // other implementation
+	  extractedMethod(edgeList, p);
+      return null;
+   }
+
+   // TODO: Your answer
+   <T extends Graph> void extractedMethod(List<T> objs, String p) {
+    	for (T obj : objs) {
+			if (obj.contains(p))
+				System.out.println(obj);
+		}
+    }
+}
+
+class Graph {
+	String name;
+	boolean contains(String p) {
+		return name.contains(p);
+	}
+}
+
+class Node extends Graph{
+   String name;
+
+   public boolean contains(String p) {
+      return name.contains(p);
+   }
+}
+
+class Edge extends Graph{
+   String name;
+
+   public boolean contains(String p) {
+       return name.contains(p);
+   }
+}
